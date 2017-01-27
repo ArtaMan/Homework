69ae4d7191423bc08ca306007f65746256e31471
ArAmAn <artem.amosov@mail.ru>
2017-01-27 22:03:17 +0300

    added cityes

---
+++ Gusarov.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+import sys
+inpu = open('NCities2.txt', 'r', encoding='utf8')
+
+a = inpu.readlines()
+for i in range(len(a)):
+    cur = ''
+    for j in a[i]:
+        if j.isalpha():
+            cur += j
+    cur = cur.lower()
+    a[i] = cur
+
+
+def game(iniortown):
+    
+
+
+    # iniortown.split()
+    town = ''
+    for i in iniortown:
+        i = str(i)
+        if i.isalpha():
+            town += i
+
+
+    town = town.lower()
+    #town += ' '
+    # town += '\n'
+
+    if town not in a:
+        print('Я такого города не знаю, либо он уже был')
+        return -1
+
+    lastb = False
+    x = 1
+    town2 = []
+    for i in range(len(town) - 1):
+        town2.append(town[i])
+
+    while lastb == False:
+        if town2[len(town2) - x] == 'ъ' or town2[len(town2) - x] == 'ь' or town2[len(town2) - x] == 'ы':
+            town2[len(town2) - x] = ' '
+        else:
+            lastb = True
+        x += 1
+    town = ''
+
+    for i in town2:
+        i = str(i)
+        if i.isalpha():
+            town += i
+
+    '''    iniortown = input()
+        iniortown.split()
+        town = ''
+        for i in iniortown:
+            i = str(i)
+            if i.isalpha():
+                town += i
+        for i in a:
+            i = str(i)
+            cor = i.lower()
+            if cor == town:
+                ok = True
+    '''
+
+
+
+    for i in range(len(a)):
+
+        a[i] = str(a[i])
+
+        cor = a[i].lower()
+
+        if cor[0] == town[len(town)-1]:
+            myTown = a[i]
+            a.remove(a[i])
+            ret = ''
+            for j in range(len(myTown)-1):
+                ret += myTown[j] 
+
+            return (ret)
+    print('Я сдаюсь')
+    sys.exit()  
+
+if __name__ == '__main__':
+    while True:
+        print(city(input()))
---
+++ amosov.py
@@ -0,0 +1,48 @@
+from random import *
+
+
+def game(city):
+    city = city.lower()
+    # if lastcity != -1:
+    #     lastcity = lastcity.lower()
+    if city not in a:
+        print('There`s no city named', city, 'or I don`t have it in my cities...')
+        return -1
+    if city not in used[ord(city[0].lower()) - 1072]:
+        used[ord(city[0].lower()) - 1072].append(city)
+    else:
+        print('This city already was')
+        return -1
+    # if lastcity != -1:
+    #     if lastcity[-1].lower() != city[0].lower():
+    #         print('Choice city that starts on letter "', lastcity[-1], '"', sep='')
+    #         return lastcity
+    last = city[-1].lower()
+    if last in ['ь', 'ъ', 'ы']:
+        print('Theres no city on the last letter')
+        return -1
+    temp = ord(last) - 1072
+    if len(used[ord(city[-1].lower()) - 1072]) == len(words[ord(city[-1].lower()) - 1072]):
+        print('sdayus')
+        exit()
+    lol = choice(words[temp])
+    while lol in used:
+        lol = choice(words[temp])
+    used[ord(lol[0].lower()) - 1072].append(lol)
+    lol = lol[0].upper() + lol[1:len(lol) - 1]
+    for i in range(len(lol) - 1):
+        if lol[i] == '-' or lol[i] == ' ':
+            lol = lol[0:i + 1] + lol[i + 1].upper() + lol[i + 2:]
+    return lol
+
+fin = open('citiesAmosov.txt', 'r', encoding='utf-8')
+a = fin.readlines()
+for i in range(len(a)):
+    a[i] = a[i][:len(a[i]) - 1]
+    a[i] = a[i].lower()
+words = [[] for i in range(32)]
+for i in range(len(a)):
+    temp = ord(a[i][0])
+    temp1 = temp - 1072
+    words[temp1].append(a[i])
+used = [[] for i in range(32)]
