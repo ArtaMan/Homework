7eb7596baac0c7a0d73d1c5138229050816b7dd1
ArAmAn <artem.amosov@mail.ru>
2017-01-27 22:30:32 +0300

    Added cityes test file

---
+++ testityes.py
@@ -0,0 +1,12 @@
+import amosov as player1
+import Gusarov as player2
+import time
+
+
+word2 = 'Москва'
+while True:
+    word1 = player1.game(word2)
+    print(word1)
+    word2 = player2.game(word1)
+    print(word1, word2)
+    # time.sleep(1)
