fb9f8a58bdbde0e84ec8cad27da22e3c097e9c6f
ArAmAn <artem.amosov@mail.ru>
2017-01-30 13:58:14 +0300

    Made cost 1 mashine and full mashines

--- csvhomework/csvhomework.py
+++ csvhomework/csvhomework.py
@@ -4,5 +4,13 @@ with open('parkings.csv', 'r', encoding='cp1251') as file:
     fields = ["ID", "ParkingName", "global_id", "ParkingZoneNumber", "AdmArea", "District", "Address", "WorkingHours",
               "Price", "CarCapacity", "Longitude_WGS84", "Latitude_WGS84", "Coordinates", "geoData"]
     reader = csv.DictReader(file, fields, delimiter=';')
+    sum1mashine = 0
+    sumfull = 0
+    i = 0
     for row in reader:
-        print(row)
+        if i == 0:
+            i += 1
+            continue
+        sum1mashine += float(row.get('Price')) * float(row.get('CarCapacity'))
+        sumfull += float(row.get('Price')) * float(row.get('CarCapacity'))
+print(sum1mashine, sumfull)
