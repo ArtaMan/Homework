fa5a29ec433c9fe5309b9a3da531de3ea3a8e638
ArAmAn <artem.amosov@mail.ru>
2017-02-01 10:54:04 +0300

    Reshatel2000 v3.11

--- Reshatel2000.py
+++ Reshatel2000.py
@@ -6,19 +6,19 @@ import sys
 
 def _pressOffline(event):
     try:
-        a1 = int(a.get('1.0', END))
+        a1 = int(a.get())
     except:
         solve['text'] = 'Введите в строку A только цифры'
         return
 
     try:
-        b1 = int(b.get('1.0', END))
+        b1 = int(b.get())
     except:
         solve['text'] = 'Введите в строку B только цифры'
         return
 
     try:
-        c1 = int(c.get('1.0', END))
+        c1 = int(c.get())
     except:
         solve['text'] = 'Введите в строку C только цифры'
         return
@@ -59,7 +59,7 @@ def _pressOffline(event):
         text += 'x1 * x2 = c / a = ' + str(p) + '\n'
         text += 'x1 + x2 = -(b / a) = ' + str(-q) + '\n\n'
         if D2 < 0:
-            text += 'У этого кравнения нет корней\n'
+            text += 'У этого уравнения нет корней\n'
         elif D2 == 0:
             if (-b1 + sqrt(D2)) / (2 * a1) == 0:
                 text += 'x = 0\n'
@@ -70,22 +70,21 @@ def _pressOffline(event):
             text += 'x2 = ' + str((-b1 - sqrt(D2)) / (2 * a1)) + '\n'
     solve['text'] = text
 
-
 def _pressOnline(event):
     try:
-        a1 = int(a.get('1.0', END))
+        a1 = int(a.get())
     except:
         solve['text'] = 'Введите в строку A только цифры'
         return
 
     try:
-        b1 = int(b.get('1.0', END))
+        b1 = int(b.get())
     except:
         solve['text'] = 'Введите в строку B только цифры'
         return
 
     try:
-        c1 = int(c.get('1.0', END))
+        c1 = int(c.get())
     except:
         solve['text'] = 'Введите в строку C только цифры'
         return
@@ -109,19 +108,16 @@ if sys.platform == 'win32':
     root.geometry('400x400+0+0')
     description = Label(root, bg='white', text='Решатель2000 решает квадратные уравнения вида ax² + bx + c = 0\n '
                                                'Введите ниже коэфиценты a, b и c', width=52, height=2)
-    a = Text(root, bg='white', height=1, width=20, bd=0)
-    b = Text(root, bg='white', height=1, width=20, bd=0)
-    c = Text(root, bg='white', height=1, width=20, bd=0)
-    a.insert('1.0', '1')
-    b.insert('1.0', '1')
-    c.insert('1.0', '1')
+    a = Entry(root, width=20, bg='white')
+    b = Entry(root, width=20, bg='white')
+    c = Entry(root, width=20, bg='white')
     # xyz = Label(root, text='ax² + bx + c = 0')
     la = Label(root, bg='white', text='A   = ', width=4, height=1)
     lb = Label(root, bg='white', text='B   = ', width=4, height=1)
     lc = Label(root, bg='white', text='C   = ', width=4, height=1)
     var = IntVar()
     DorV = Label(root, bg='white', text='Как решить уравнение?')
-    Disk = Radiobutton(root, bg='white', text='Через дискрименанту', variable=var, value=0)
+    Disk = Radiobutton(root, bg='white', text='Через дискриминант', variable=var, value=0)
     Viet = Radiobutton(root, bg='white', text='Через теорему Виета', variable=var, value=1)
     pressOffline = Button(root, bg='white', text='Решить локально', width=15, height=1, bd=1)
     pressOnline = Button(root, bg='white', text='Решить онлайн', width=15, height=1, bd=1)
@@ -129,6 +125,7 @@ if sys.platform == 'win32':
     kostil = Label(root, bg='white', text='', width=0, height=0)
     kostil2 = Label(root, bg='white', text='', width=0, height=0)
     kostil3 = Label(root, bg='white', text='', width=0, height=0)
+    kostil4 = Label(root, text='', width=0, height=0)
 
     description.grid(row=0, column=0)
     la.grid(row=2, column=0)
@@ -137,38 +134,37 @@ if sys.platform == 'win32':
     a.place(x=205, y=36)
     b.place(x=205, y=58)
     c.place(x=205, y=81)
-    DorV.grid(row=5, column=0)
-    Disk.place(x=20, y=125)
-    Viet.place(x=235, y=125)
-    kostil.grid(row=7, column=0)
-    kostil2.grid(row=8, column=0)
-    kostil3.grid(row=9, column=0)
-    pressOffline.place(x=15, y=145)
-    pressOnline.place(x=265, y=145)
-    solve.grid(row=10, column=0)
+    kostil4.grid(row=5, column=0)
+    DorV.grid(row=6, column=0)
+    Disk.place(x=20, y=145)
+    Viet.place(x=235, y=145)
+    kostil.grid(row=8, column=0)
+    kostil2.grid(row=9, column=0)
+    kostil3.grid(row=10, column=0)
+    pressOffline.place(x=15, y=165)
+    pressOnline.place(x=265, y=165)
+    solve.grid(row=11, column=0)
 else:
     root.title('Reshatel2000')
     description = Label(root, text='Решатель2000 решает квадратные уравнения вида ax² + bx + c = 0\n '
                                    'Введите ниже коэфиценты a, b и c', width=52, height=2)
-    a = Text(root, height=1, width=20)
-    b = Text(root, height=1, width=20)
-    c = Text(root, height=1, width=20)
-    a.insert('1.0', '1')
-    b.insert('1.0', '1')
-    c.insert('1.0', '1')
+    a = Entry(root, width=20, text='1')
+    b = Entry(root, width=20)
+    c = Entry(root, width=20)
     # xyz = Label(root, text='ax² + bx + c = 0')
     la = Label(root, text='A   = ', width=4, height=1)
     lb = Label(root, text='B   = ', width=4, height=1)
     lc = Label(root, text='C   = ', width=4, height=1)
     var = IntVar()
     DorV = Label(root, text='Как решить уравнение?')
-    Disk = Radiobutton(root, text='Через дискрименанту', variable=var, value=0)
+    Disk = Radiobutton(root, text='Через дискриминант', variable=var, value=0)
     Viet = Radiobutton(root, text='Через теорему Виета', variable=var, value=1)
     pressOffline = Button(root, text='Решить локально', width=15, height=1)
     pressOnline = Button(root, text='Решить онлайн', width=15, height=1)
     solve = Label(root, text='', width=50, height=10)
     kostil = Label(root, text='', width=0, height=0)
     kostil2 = Label(root, text='', width=0, height=0)
+    kostil3 = Label(root, text='', width=0, height=0)
 
     description.grid(row=0, column=0)
     la.grid(row=2, column=0)
@@ -177,13 +173,14 @@ else:
     a.place(x=260, y=36)
     b.place(x=260, y=58)
     c.place(x=260, y=81)
-    DorV.grid(row=5, column=0)
-    Disk.place(x=45, y=125)
-    Viet.place(x=260, y=125)
+    kostil3.grid(row=5, column=0)
+    DorV.grid(row=6, column=0)
+    Disk.place(x=45, y=145)
+    Viet.place(x=260, y=145)
     kostil.grid(row=7, column=0)
     kostil2.grid(row=8, column=0)
-    pressOffline.place(x=30, y=145)
-    pressOnline.place(x=280, y=145)
+    pressOffline.place(x=30, y=165)
+    pressOnline.place(x=280, y=165)
     solve.grid(row=9, column=0)
 pressOffline.bind('<Button-1>', _pressOffline)
 pressOnline.bind('<Button-1>', _pressOnline)
